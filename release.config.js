00b8c6650d418fe08a532bb85674d91fc486b8f1
Rinat Zaripov <zaripov.rinat@gmail.com>
2019-10-24 23:22:29 +1100

    ci: add travis

--- release.config.js
+++ release.config.js
@@ -5,14 +5,6 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json'],
-        message:
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
     '@semantic-release/github',
   ],
 };
